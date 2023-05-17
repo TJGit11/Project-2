a96e4d0247cbfdf7ca2bc6c2bf61aa2e1804ba32
benjiwildcat <benjiwildcat@gmail.com>
2023-05-17 12:49:31 -0500

    customer models

--- applyRoutes.js
+++ applyRoutes.js
@@ -1,3 +1,5 @@
+const { application } = require("express");
+
 router.post('/apply', withAuth, async (req, res) => {
     try {
         const newApplication = await application.create({
@@ -10,3 +12,24 @@ router.post('/apply', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+
+// router.delete('/apply', withAuth, async (req, res) => {
+//     try {
+//       const projectData = await application.destroy({
+//         where: {
+//           id: req.params.id,
+//           user_id: req.session.user_id,
+//         },
+//       });
+  
+//       if (!applicationData) {
+//         res.status(404).json({ message: 'No application found with this id!' });
+//         return;
+//       }
+  
+//       res.status(200).json(projectData);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
---
+++ models/Customer.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+Customer.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        order: {
+            type: DataTypes.STRING,
+        },
+    }
+)
+
+module.export = Customer
