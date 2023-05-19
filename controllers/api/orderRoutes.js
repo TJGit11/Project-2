08e758e24c85fcbe9c297c84a2df53c10356584d
unknown <thomasjjohnson9311@gmail.com>
2023-05-18 20:12:24 -0500

    checkout-handlebars

--- controllers/api/orderRoutes.js
+++ controllers/api/orderRoutes.js
@@ -1,15 +1,18 @@
-
-const { Order } = require("../../models");
+const router = require('express').Router();
+const { Order } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/order', withAuth, async (req, res) => {
-    try {
-        const newOrder = await Order.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
+  try {
+    const newOrder = await Order.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-        res.status(200).json(newOrder);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+    res.status(200).json(newOrder);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
+module.exports = router;
